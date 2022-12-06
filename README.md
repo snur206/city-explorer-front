bbeca40563e8959a5b5dca3a1f8220c89e7da155
SHARMARKE NUR <Snur206@yahoo.com>
2022-12-05 20:06:46 -0800

    tasks 1-3 done

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
 # city-explorer-front
-City Explorer UI
-first commit intial
+# Project Name
+
+**Author**: Sharmarke Nur
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+
+## Credit and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+
+Name of feature: 1. Set up your React repository & API keys.
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: 2:10
+
+Finish time: 3:45
+
+Actual time needed to complete: 1 hour 35 minutes
+
+Name of feature: 2. Locations
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 3:50
+
+Finish time: 6:25
+
+Actual time needed to complete: 2 hours 25 minutes
+
+Name of feature: 3. Map
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 6:30
+
+Finish time: 7:40
+
+Actual time needed to complete: 1 hour 10 minutes
+
+Name of feature: 4. Errors
+
+Estimate of time needed to complete:
+
+Start time:
+
+Finish time:
+
+Actual time needed to complete:
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,45 +1,42 @@
-import React from 'react';
-import map from '../images/map.png';
-import restaurantData from '../data/restaurants.json';
 import locationData from '../data/location.json';
+// import axios from 'axios';
+// const ACCESS_KEY = process.env.KEY_ACCESS_CODE;
 
-class Search extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      locationSearch: '',
-      restaurantData: restaurantData,
-      locationData: locationData
-    }
-  }
+// class Search extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       locationSearch: '',
+//       restaurantData: restaurantData,
+//       locationData: locationData
+//     }
+//   }
 
-  handleLocationSearch = (e) => {
-    e.preventDefault();
-    this.setState({
-      locationSearch: e.target.search.value
-    })
-  }
+//   // this should query location IQ for geolocation data
 
-  render() {
-    console.log(this.state);
-    return (
-      <div id="city-search">
-        <form onSubmit={this.handleLocationSearch}>
-          <label>Search for a location</label>
-          <input type="text" name="search" placeholder="Enter City here"/>
-          <button type="submit">Explore!</button>
-        </form>
-        {this.state.locationSearch && this.state.locationData
-          ? <div id="map"><img src={map} alt="location map"/></div>
-          : null
-        }
-        {this.state.locationSearch && this.state.restaurantData
-          ? <ul>{this.state.restaurantData.map(place => <li>{place.restaurant}</li>)}</ul>
-          : null
-        }
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div id="city-search">
+//         <form>
+//           <label>Search for location</label>
+//           <input type="text" name="search" placeholder="Enter City here"/>
+//           <button type="submit">Explore!</button>
+//         </form>
+//         {this.state.locationData 
+//           ? <p>{this.state.locationData.display_name}</p>
+//           : <p>Search for a city!</p>
+//         }
+//         {this.state.locationSearch && this.state.locationData
+//           ? <div id="map"><img src={map} alt="location map"/></div>
+//           : null
+//         }
+//         {this.state.locationSearch && this.state.restaurantData
+//           ? <ul>{this.state.restaurantData.map(place => <li>{place.restaurant}</li>)}</ul>
+//           : null
+//         }
+//       </div>
+//     )
+//   }
+// }
 
-export default Search;
+// export default Search;
